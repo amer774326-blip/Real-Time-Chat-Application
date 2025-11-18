bb5db5742104ae4db5461c691b2e2209083de658
amer774326-blip <amer774326@gmail.com>
2025-11-19 02:33:35 +0300

    Modify mongoose strictQuery and logging messages
    Updated mongoose strictQuery setting and improved database connection logging.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//external dependencies
 const express = require("express");
 const http = require("http");
 const dotenv = require("dotenv");
@@ -7,7 +6,6 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const moment = require("moment");
 
-//internal dependencies
 const loginRouter = require("./router/loginRouter");
 const usersRouter = require("./router/usersRouter");
 const inboxRouter = require("./router/inboxRouter");
@@ -16,52 +14,44 @@ const {
   errorHandler,
 } = require("./middlewares/common/errorHandler");
 
-//app initialization
 const app = express();
 const server = http.createServer(app);
 dotenv.config();
 
-//socket creation
 const io = require("socket.io")(server);
 global.io = io;
 
-//setmoment as application locals
 app.locals.moment = moment;
 
-//database connection
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", true);
 mongoose
   .connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("database connection successfull"))
-  .catch((err) => console.log(err));
+  .then(() => console.log("Database connection successful!"))
+  .catch((err) => console.error("Database connection error:", err));
 
-// request parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//set view engine
 app.set("view engine", "ejs");
 
-//set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-//parse cookies
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-//routing setup
 app.use("/", loginRouter);
 app.use("/users", usersRouter);
 app.use("/inbox", inboxRouter);
 
-//404 not found handler
 app.use(notFoundHandler);
 
-//common error handler
 app.use(errorHandler);
 
 server.listen(process.env.PORT, () => {
   console.log(`App is listening to port: ${process.env.PORT}`);
 });
+```http://googleusercontent.com/generated_image_content/0
+
+
