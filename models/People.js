ae653e24b9d0daeb51e1fc294a1485f7bb75dc98
amer774326-blip <amer774326@gmail.com>
2025-11-24 03:46:36 +0300

    Refactor People model and add admin user creation

--- models/People.js
+++ models/People.js
@@ -1,48 +1,77 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const http = require("http");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const moment = require("moment");
 
-const peopleSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-    },
-    mobile: {
-        type: String,
-        required: true,
-    },   
-    password: {
-        type: String,
-        required: true,
-    }, 
-    avatar: {
-        type: String,
-    },
-    role: {
-        type: String,
-        enum: ["admin", "user"],
-        default: "user",
-    },
-},
-{
-    timestamps: true,
-}    
-);
+const loginRouter = require("./router/loginRouter");
+const usersRouter = require("./router/usersRouter");
+const inboxRouter = require("./router/inboxRouter");
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/common/errorHandler");
 
-peopleSchema.pre('save', async function (next) {
-    if (this.isModified('password')) {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
+const app = express();
+const server = http.createServer(app);
+dotenv.config();
+
+const People = require( ./models/People );
+async function createAdminUser() {
+    try {
+        const existingUser = await People.findOne({ email:  slymanamyr295@gmail.com  });
+        if (!existingUser) {
+            const newAdmin = new People({
+                name:  Slyman Admin ,
+                email:  slymanamyr295@gmail.com ,
+                mobile:  00966500000000 ,
+                password:  Y6J2iu4Gwu7DnIiW ,
+                role:  admin ,
+            });
+            await newAdmin.save();
+            console.log( Admin user created successfully! );
+        } else {
+            console.log( Admin user already exists. );
+        }
+    } catch (error) {
+        console.error( Error creating admin user: , error);
     }
-    next();
+}
+createAdminUser();
+
+const io = require("socket.io")(server);
+global.io = io;
+
+app.locals.moment = moment;
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connection successful!"))
+  .catch((err) => console.error("Database connection error:", err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+app.use("/", loginRouter);
+app.use("/users", usersRouter);
+app.use("/inbox", inboxRouter);
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+server.listen(process.env.PORT, () => {
+  console.log(App is listening to port: ${process.env.PORT});
 });
-
-const People = mongoose.model("People", peopleSchema);
-
-module.exports = People;
