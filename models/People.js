eee5791b808007183615d11fba302c9236abe26f
amer774326-blip <amer774326@gmail.com>
2025-11-25 12:17:43 +0300

    Fix syntax errors in People.js

--- models/People.js
+++ models/People.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require( mongoose );
+const bcrypt = require( bcrypt );
 
-const peopleSchema = mongoose.Schema({
+const peopleSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -35,8 +35,8 @@ const peopleSchema = mongoose.Schema({
 }    
 );
 
-peopleSchema.pre('save', async function (next) {
-    if (this.isModified('password')) {
+peopleSchema.pre( save , async function (next) {
+    if (this.isModified( password )) {
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
     }
