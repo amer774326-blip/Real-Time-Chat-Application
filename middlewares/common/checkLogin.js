5c9190630a9cf2657179ebe5edafe587257b53c0
amer774326-blip <amer774326@gmail.com>
2025-12-03 01:24:39 +0300

    Update checkLogin.js

--- middlewares/common/checkLogin.js
+++ middlewares/common/checkLogin.js
@@ -6,6 +6,36 @@ function checkLogin(req, res, next) {
   }
 }
 
+function redirectLoggedIn(req, res, next) {
+  if (req.signedCookies.chat_application_cookie) {
+    res.redirect("/inbox");
+  } else {
+    next();
+  }
+}
+
+function requireRole(role) {
+  return (req, res, next) => {
+    if (req.user.role && role.includes(req.user.role)) {
+      next();
+    } else {
+      if (res.locals.html) {
+        res.redirect("/inbox");
+      } else {
+        res.status(401).json({
+          errors: {
+            common: {
+              msg: "You are not authorized to access this page!",
+            },
+          },
+        });
+      }
+    }
+  };
+}
+
 module.exports = {
   checkLogin,
+  redirectLoggedIn,
+  requireRole,
 };
