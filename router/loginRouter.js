a433500fc6f1b5361dc64c707f54a1340d8b9988
amer774326-blip <amer774326@gmail.com>
2025-12-04 03:34:07 +0300

    Update loginRouter.js

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -1,14 +1,18 @@
-// external dependencies
-const express = require("express");
+// loginRouter.js
+const express = require('express');
 
 // internal dependencies
-const { getLogin, login, logout } = require("../controller/loginController");
-const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const { getLogin, login, logout } = require('../Controller/LoginController');
+const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse');
+
 const {
   doLoginValidators,
   doLoginValidationHandler,
-} = require("../middlewares/login/loginValidators");
-const { redirectLoggedIn } = require("../middlewares/common/checkLogin");
+} = require('../middlewares/login/loginValidators');
+
+const {
+  redirectLoggedIn,
+} = require('../middlewares/common/checkLogin');
 
 const router = express.Router();
 
